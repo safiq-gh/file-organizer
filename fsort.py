dc2f3092d0f0a0f6e6505b327d96873d888ae66d
safiq-gh <230081601045@crescent.education>
2026-01-18 16:22:11 +0530

    V1: add argparse CLI and remove interactive input

--- fsort.py
+++ fsort.py
@@ -1,8 +1,9 @@
-from fsort import scanner,grouper,mover,preview
+from fsort import scanner, grouper, mover, preview, cli
 def main():
 
     # ask user for directory path
-    path = input("Enter path: ")
+    args = cli.parse_args()
+    path = args.path
     try:
         # scan directory
         files = scanner.scan_directory(path)
@@ -19,22 +20,20 @@ def main():
     #print(groups)
 
     # preview planned moves
-    pc = 0
-    for directory in groups:
-        if len(groups[directory]) == 0:
-            pc += 1
-    if pc < len(groups):
-        preview.preview_moves(groups)
+    if args.dry_run:
+        pc = 0
+        for directory in groups:
+            if len(groups[directory]) == 0:
+                pc += 1
+        if pc < len(groups):
+            print(">>> DRY RUN MODE: No files will be moved.")
+            preview.preview_moves(groups)
+        else:
+            print("No preveiw to show!. Files are already in correct order.")
+            return
     else:
-        print("No preveiw to show!. Files are already in correct order.")
-        return
-    # ask for confirmation
-    confirmation = input("Confirm to change(y/n): ").lower()
-    if confirmation == "y":
         # apply moves if confirmed
         mover.apply_moves(groups, path)
-    else:
-        return
 
 if __name__ == "__main__":
     main()
--- fsort/cli.py
+++ fsort/cli.py
@@ -1,2 +1,8 @@
 # CLI parsing will be added in V1 (argparse)
+import argparse 
 
+def parse_args():
+    parser = argparse.ArgumentParser(description="File Organizer")
+    parser.add_argument("--path", required=True, type=str, help="Path to the folder you want to organize.")
+    parser.add_argument("--dry-run", action="store_true", help="Preview changes without moving files")
+    return parser.parse_args()
