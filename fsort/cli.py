116bf4be41eedc5d9b9612712e556420adb59f87
safiq-gh <230081601045@crescent.education>
2026-01-21 00:28:28 +0530

    V1: add optional recursive scanning

--- fsort/cli.py
+++ fsort/cli.py
@@ -5,4 +5,5 @@ def parse_args():
     parser = argparse.ArgumentParser(description="File Organizer")
     parser.add_argument("--path", required=True, type=str, help="Path to the folder you want to organize.")
     parser.add_argument("--dry-run", action="store_true", help="Preview changes without moving files")
+    parser.add_argument("--recursive", action="store_true", help="Scan the directories and sub-directories recursively.")
     return parser.parse_args()
--- fsort/scanner.py
+++ fsort/scanner.py
@@ -1,7 +1,14 @@
-def scan_directory(path):
+def scan_directory(path, recursive=False):
     files = []
-    for fildir in path.iterdir():
-        if fildir.is_file() and fildir.name not in ["organizer.py", "fsort.py"] and not fildir.name.startswith("."):
-            files.append(fildir)
+    if recursive:
+        for fildir in path.iterdir():
+            if fildir.is_file() and fildir.name not in ["organizer.py", "fsort.py"] and not fildir.name.startswith("."):
+                files.append(fildir)
+            elif fildir.is_dir() and fildir.name not in ["Images", "Docs", "Others", "fsort"] and not fildir.name.startswith("."):
+                files.extend(scan_directory(fildir, recursive=True))
+    else:
+        for fildir in path.iterdir():
+            if fildir.is_file() and fildir.name not in ["organizer.py", "fsort.py"] and not fildir.name.startswith("."):
+                files.append(fildir)
     return files
 
