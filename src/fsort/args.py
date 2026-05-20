b5cdcfd7e4ec119497462a3d38f605201591c028
Safiq-gh <230081601045@crescent.education>
2026-05-20 09:47:54 +0000

    V2-Done

--- src/fsort/args.py
+++ src/fsort/args.py
@@ -1,8 +1,22 @@
-import argparse 
+import argparse
 
 def parse_args():
     parser = argparse.ArgumentParser(description="File Organizer")
-    parser.add_argument("--path", required=True, type=str, help="Path to the folder you want to organize.")
-    parser.add_argument("--dry-run", action="store_true", help="Preview changes without moving files")
-    parser.add_argument("--recursive", action="store_true", help="Scan the directories and sub-directories recursively.")
+    parser.add_argument(
+        "--path",
+        required=True,
+        type=str,
+        help="Path to the folder you want to organize."
+    )
+    parser.add_argument(
+        "--dry-run",
+        action="store_true",
+        help="Preview changes without moving files"
+    )
+    parser.add_argument(
+        "--recursive",
+        action="store_true",
+        help="Scan directories recursively"
+    )
+
     return parser.parse_args()
--- src/fsort/cli.py
+++ src/fsort/cli.py
@@ -1,7 +1,28 @@
 from pathlib import Path
 from fsort import scanner, grouper, mover, preview, args
 
+
+def show_banner():
+    PURPLE = "\033[95m"
+    CYAN = "\033[96m"
+    GREEN = "\033[92m"
+    RESET = "\033[0m"
+
+    banner = r"""
+███████╗██╗██╗     ███████╗███████╗ ██████╗ ██████╗ ████████╗
+██╔════╝██║██║     ██╔════╝██╔════╝██╔═══██╗██╔══██╗╚══██╔══╝
+█████╗  ██║██║     █████╗  ███████╗██║   ██║██████╔╝   ██║
+██╔══╝  ██║██║     ██╔══╝  ╚════██║██║   ██║██╔══██╗   ██║
+██║     ██║███████╗███████╗███████║╚██████╔╝██║  ██║   ██║
+╚═╝     ╚═╝╚══════╝╚══════╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝   ╚═╝
+
+        FileSort — Organize Your Files
+"""
+    print(f"{CYAN}{banner}{RESET}")
+
 def main():
+    show_banner()  # <-- banner runs immediately
+
     args_ns = args.parse_args()
     path = Path(args_ns.path)
 
